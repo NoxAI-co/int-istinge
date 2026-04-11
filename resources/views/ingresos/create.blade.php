48b449fcfc5279ea6cadc7a7d8d3553d5a3a7c33
higuita1498 <higuitajokers@gmail.com>
2026-04-11 08:24:29 -0500

    fix: reorder input-helper element to fix radio button layout in ingresos create view

--- resources/views/ingresos/create.blade.php
+++ resources/views/ingresos/create.blade.php
@@ -503,8 +503,8 @@
           <div class="form-radio">
             <label class="form-check-label" style="font-size: 13px;">
               <input type="radio" class="form-check-input" name="tipo_electronica" value="2">Convertir a electrónica y emitir
-              <a><i data-tippy-content="si la factura ya es electronica se hara uso de la funcion de emitir a la DIAN directamente." class="icono far fa-question-circle"></i></a>
               <i class="input-helper"></i>
+              <a><i data-tippy-content="si la factura ya es electronica se hara uso de la funcion de emitir a la DIAN directamente." class="icono far fa-question-circle"></i></a>
             </label>
           </div>
         </div>
