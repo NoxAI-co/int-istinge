c8ebd7a40f156d909b40749b345f0d095acdd37d
higuita1498 <higuitajokers@gmail.com>
2026-04-20 16:37:29 -0500

    feat: restrict access to contract details by verifying permission 860 in show method

--- app/Http/Controllers/ContratosController.php
+++ app/Http/Controllers/ContratosController.php
@@ -2664,6 +2664,9 @@ class ContratosController extends Controller
 
     public function show($id)
     {
+        if (!isset($_SESSION['permisos']['860'])) {
+            return redirect()->back()->with('danger', 'No cuenta con los permisos necesarios para realizar esta acción');
+        }
         $this->getAllPermissions(Auth::user()->id);
         view()->share(['middel' => true]);
         $inventario = false;
