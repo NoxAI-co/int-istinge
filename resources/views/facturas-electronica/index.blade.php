20e55219da51b5964c4ba2858d5ce03ad6d20d46
higuita1498 <higuitajokers@gmail.com>
2026-05-02 13:31:04 -0500

    feat: enable display of electronic invoice missing reports alert

--- resources/views/facturas-electronica/index.blade.php
+++ resources/views/facturas-electronica/index.blade.php
@@ -91,7 +91,7 @@
 
     @endif
 
-    @if(isset($reporteFaltantes) && !empty($reporteFaltantes['faltantes']) && 1==2)
+    @if(isset($reporteFaltantes) && !empty($reporteFaltantes['faltantes']))
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
         <button type="button" class="close d-none d-md-block" data-dismiss="alert" aria-label="Cerrar">
             <span aria-hidden="true">&times;</span>
